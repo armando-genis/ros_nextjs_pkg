e9da4b1dd0e711efa5e2f867f9994db2d176bfbd
Armando Genis <83677824+armando-genis@users.noreply.github.com>
2023-09-06 13:12:21 -0600

    conection with points_raw correctly

--- dashboard_app/data/RosComponent.js
+++ dashboard_app/data/RosComponent.js
@@ -5,10 +5,11 @@ const ROSLIB = require('roslib');
 function RosComponent() {
     const [ros, setRos] = useState(null);
     const [connected, setConnected] = useState(false);
+    const [pointCloudData, setPointCloudData] = useState(null);
 
     useEffect(() => {
         const rosInstance = new ROSLIB.Ros({
-            url: 'ws://localhost:9090' // Make sure the rosbridge_server is running on this address.
+            url: 'ws://localhost:9090'
         });
 
         rosInstance.on('connection', () => {
@@ -25,14 +26,38 @@ function RosComponent() {
             setConnected(false);
         });
 
+        if (connected) {
+            const pointCloudTopic = new ROSLIB.Topic({
+                ros: rosInstance,
+                name: '/points_raw',
+                messageType: 'sensor_msgs/PointCloud2'
+            });
+
+            pointCloudTopic.subscribe(function (message) {
+                console.log('Received message on /points_raw:', message);
+                setPointCloudData(message);
+            });
+
+            // Cleanup
+            return () => {
+                pointCloudTopic.unsubscribe();
+            };
+        }
+
         setRos(rosInstance);
-    }, []);
+    }, [connected]);
 
     return (
         <div>
             {connected ? "Connected to ROS!" : "Not connected to ROS!"}
+            <div>
+                Last Point Cloud Data Timestamp:
+                {pointCloudData ? `${pointCloudData.header.stamp.sec}s ${pointCloudData.header.stamp.nanosec}ns` : "No data yet"}
+
+            </div>
         </div>
     );
 }
 
 export default RosComponent;
+
